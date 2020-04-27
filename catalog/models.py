059f41ac2d61c366867a23e91dbbca5b34511b69
FatemeMoslemi <moslemi1998@gmail.com>
2020-04-27 07:55:47 +0430

    model data

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a genre book (e.g. science fiction, french poetry , etc)')
+
+    def __str__(self):
+        return self.name
+
+class book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    summary = models.TextField(max_length=1000, help_text='Enter a description')
+    isbn = models.CharField('isbn', max_length=200, help_text='')
